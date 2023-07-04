6333db512fd337cc84d7aaa0b28ed8164060cfd2
chafferexb <92236807+chafferexb@users.noreply.github.com>
2023-07-04 15:22:25 +0800

    Update NoteOp.java
    完成关键字搜索和根据多个tag搜索

--- src/main/java/com/example/stellarmemo/service/NoteOp.java
+++ src/main/java/com/example/stellarmemo/service/NoteOp.java
@@ -7,4 +7,6 @@ public interface NoteOp {
     public WebResult createNote(String user_id,String content,String note_id, String imageSrc);
     public WebResult modifyNote(String note_id,String content);
     public WebResult deleteNote(String note_id);
+    public List<Note> searchByKey(String key,int startIndex,int pageSize);
+    public List<Note> searchByTag(String tag1,String tag2,String tag3);
 }
