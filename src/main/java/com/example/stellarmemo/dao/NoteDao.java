d117daedcba94a969bd7328f574bf3dbfa934603
chafferexb <92236807+chafferexb@users.noreply.github.com>
2023-07-04 15:21:44 +0800

    Update NoteDao.java
    完成关键字搜索和根据多个tag搜索

--- src/main/java/com/example/stellarmemo/dao/NoteDao.java
+++ src/main/java/com/example/stellarmemo/dao/NoteDao.java
@@ -17,4 +17,8 @@ public interface NoteDao {
                            @Param("content") String content);//修改笔记
 
     public void deleteNote(@Param("note_id") String note_id);//删除笔记
+    public List<Note> searchByKey(@Param("key") String key,@Param("startIndex") int startIndex,@Param("pageSize") int pageSize);
+    public List<Note> searchByTag1(@Param("tag1") String tag1);
+    public List<Note> searchByTag2(@Param("tag1") String tag1,@Param("tag2") String tag2);
+    public List<Note> searchByTag3(@Param("tag1") String tag1,@Param("tag2") String tag2,@Param("tag3") String tag3);
 }
